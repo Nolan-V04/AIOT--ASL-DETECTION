dfcd4f01dac5a8bad4c70344dda638e107c938cc
Nolan <vhnam04@gmail.com>
2025-07-31 20:00:29 +0700

    fix sendImage to Nodered

--- src/sendImage.py
+++ src/sendImage.py
@@ -32,10 +32,10 @@ def encode_and_publish(image_path):
         full_base64_string = f"data:image/jpeg;base64,{encoded_string}"
         
         # Gửi thẳng chuỗi Base64 qua MQTT
-        print(f"Đang gửi dữ liệu ảnh qua topic: {TOPIC}")
+        print(f"Đang gửi dữ liệu ảnh qua topic: glove/image/data")
         # MQTT có giới hạn kích thước payload, nhưng với HiveMQ Cloud free thì khá lớn (256KB)
         # Nếu ảnh của bạn lớn hơn, hãy giảm chất lượng ảnh trước khi gửi
-        client.publish(TOPIC, full_base64_string, qos=1) # Dùng QoS 1 để đảm bảo tin nhắn được gửi
+        client.publish("glove/image/data", full_base64_string, qos=1) # Dùng QoS 1 để đảm bảo tin nhắn được gửi
         print("Gửi thành công!")
 
     except FileNotFoundError:
@@ -44,16 +44,16 @@ def encode_and_publish(image_path):
         print(f"Đã xảy ra lỗi khi gửi: {e}")
 
 # --- VÒNG LẶP CHÍNH ---
-try:
-    # Chuẩn bị 1 file ảnh tên là test_image.jpg trong cùng thư mục
-    image_to_upload = 'test_image.jpg' 
-    while True:
-        encode_and_publish(image_to_upload)
-        # Tăng thời gian chờ vì gửi dữ liệu lớn tốn nhiều tài nguyên hơn
-        time.sleep(15) 
-except KeyboardInterrupt:
-    print("\nDừng chương trình.")
-finally:
-    client.loop_stop()
-    client.disconnect()
-    print("Ngắt kết nối MQTT.")
+# try:
+#     # Chuẩn bị 1 file ảnh tên là test_image.jpg trong cùng thư mục
+#     image_to_upload = 'test_image.jpg' 
+#     while True:
+#         encode_and_publish(image_to_upload)
+#         # Tăng thời gian chờ vì gửi dữ liệu lớn tốn nhiều tài nguyên hơn
+#         time.sleep(15) 
+# except KeyboardInterrupt:
+#     print("\nDừng chương trình.")
+# finally:
+#     client.loop_stop()
+#     client.disconnect()
+#     print("Ngắt kết nối MQTT.")
